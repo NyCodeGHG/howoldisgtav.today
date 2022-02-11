1c5cbaa19e8deebe2b3b2da7247743a589912823
NyCode <me@nycode.dev>
2022-02-11 10:11:42 +0100

    Add i18n

--- components/TimeDifference.tsx
+++ components/TimeDifference.tsx
@@ -1,11 +1,24 @@
 import { intervalToDuration } from "date-fns";
+import { useTranslation } from "next-i18next";
 import { useNow } from "../hooks/now";
 import styles from '../styles/TimeDifference.module.css';
 
 function TimeDifference({ since }: { since: Date }): JSX.Element {
   const now = useNow();
+  const { t } = useTranslation('common');
   const difference = intervalToDuration({ end: now, start: since });
-  const text = `GTA V is ${difference.years} years, ${difference.months} months and ${difference.days} days old today.`;
+  const text = t('gta-v-is-old', {
+    time: t('year', {
+      count: difference.years
+    }) + ', ' +
+      t('month', {
+        count: difference.months
+      }) + ' ' +
+      t('and') + ' ' +
+      t('day', {
+        count: difference.days
+      })
+  });
   return <p className={styles.differenceText}>{text}</p>;
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import TimeDifference from '../components/TimeDifference'
 import styles from '../styles/Home.module.css'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 const GTA_V_RELEASE = new Date('September 17, 2013');
 
@@ -15,3 +16,11 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getServerSideProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common']))
+    }
+  }
+}
