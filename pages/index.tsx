cad0298ef418c84cd07e96879608ccb600fad455
NyCode <me@nycode.dev>
2022-02-11 10:50:48 +0100

    Add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,28 @@ import type { NextPage } from 'next'
 import TimeDifference from '../components/TimeDifference'
 import styles from '../styles/Home.module.css'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import Head from 'next/head';
+import { useTranslation } from 'next-i18next';
 
 const GTA_V_RELEASE = new Date('September 17, 2013');
 
 const Home: NextPage = () => {
+  const { i18n } = useTranslation();
   return (
     <div className={styles.container}>
+      <Head>
+        <title>GTA VI when?</title>
+        <meta name="description" content="Tells you how old GTA V is" />
+        <meta name="application-name" content="How old is GTA V today?" />
+        <meta property="og:title" content="How old is GTA V today?" />
+        <meta property="og:site_name" content='How old is GTA V today?' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://howoldisgtav.today' />
+        <meta property='og:description' content="It's very old. Click the link to see how old it is." />
+        <meta property='og:locale' content={i18n.language} />
+        <meta name='twitter:card' content='summary'/>
+        <meta name='twitter:creator' content='@NyCodeGHG'/>
+      </Head>
       <main className={styles.main}>
         <TimeDifference since={GTA_V_RELEASE} />
       </main>
